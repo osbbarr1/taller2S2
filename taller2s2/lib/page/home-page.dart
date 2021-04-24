6f16bef50f0b4c5aab22e0e143d02bdaa2a3aff9
osbbarr1 <osbbarr1@gmail.com>
2021-04-23 21:04:36 -0500

    Se crea homepage y botones de calculadora

---
+++ taller2s2/lib/page/home-page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculadora"),
+      ),
+      body: body(),
+    );
+  }
+
+  Widget body() {
+    return Column(
+      children: [
+        Container(
+          color: Colors.red,
+          height: 100,
+        ),
+        Container(
+          color: Colors.blue,
+          height: 100,
+        ),
+        Container(
+          color: Colors.yellow,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text("7")),
+                  ElevatedButton(onPressed: () {}, child: Text("8")),
+                  ElevatedButton(onPressed: () {}, child: Text("9")),
+                  ElevatedButton(onPressed: () {}, child: Text("/")),
+                ],
+              ),
+              Row(
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text("4")),
+                  ElevatedButton(onPressed: () {}, child: Text("5")),
+                  ElevatedButton(onPressed: () {}, child: Text("6")),
+                  ElevatedButton(onPressed: () {}, child: Text("*")),
+                ],
+              ),
+              Row(
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text("1")),
+                  ElevatedButton(onPressed: () {}, child: Text("2")),
+                  ElevatedButton(onPressed: () {}, child: Text("3")),
+                  ElevatedButton(onPressed: () {}, child: Text("-")),
+                ],
+              ),
+              Row(
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text("0")),
+                  ElevatedButton(onPressed: () {}, child: Text("C")),
+                  ElevatedButton(onPressed: () {}, child: Text("=")),
+                  ElevatedButton(onPressed: () {}, child: Text("+")),
+                ],
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
